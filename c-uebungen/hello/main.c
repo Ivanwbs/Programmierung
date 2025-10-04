9cfc3203dcbefa381a74165305504c33c39f90df
Ivanwbs <ivan@gizermail.de>
2025-10-04 10:51:11 +0200

    test txt added

---
+++ c-uebungen/hello/main.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void){
+    printf("Hallo, C auf Debian 13!\n");
+    return 0;
+}
---
+++ umschulung/main-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+    printf("Ha llo, C auf Debian 13!\n");
+       printf("  Hallo, C auf Debian 13!\n");
+
+   
+   
+    return 0;
+}
