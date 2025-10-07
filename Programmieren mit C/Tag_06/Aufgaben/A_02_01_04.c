0ba70869d8f4eeafe01e7da444d987f3c2bb6c45
IvanGizer <ivan@gizermail.de>
2025-10-07 10:31:20 +0000

    .

--- Programmieren mit C/Tag_06/Aufgaben/A_02_01_04.c
+++ Programmieren mit C/Tag_06/Aufgaben/A_02_01_04.c
@@ -2,10 +2,18 @@
 
 int main()
 {
-    int x;
+    int eingabe, zaehler = 0;
 
-
-
-
+    do
+    {
+        printf("Bitte geben Sie eine Zahl ein zum Beenden:\n");
+        fflush(stdin);
+        scanf(" %d", &eingabe);
+        zaehler = zaehler ++;
+        printf("Bisherige Summe: %d\n", zaehler);
+    }
+    while (zaehler = 0);
+    printf("Endsumme: %d\n", zaehler);
+    printf("\n\n\n");
 
 }
